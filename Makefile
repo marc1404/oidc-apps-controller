655d3a8c9d941447e218614511c0d396947f6485
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 16:47:22 +0200

    Fix copyright text: remove redundant "Copyright" prefix
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
+# SPDX-FileCopyrightText: Contributors to the Gardener project
 # SPDX-License-Identifier: Apache-2.0
 NAME                        := oidc-apps-controller
 REGISTRY                    ?= europe-docker.pkg.dev/gardener-project/snapshots/gardener/extensions
--- pkg/certificates/doc.go
+++ pkg/certificates/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
+// SPDX-FileCopyrightText: Contributors to the Gardener project
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate go tool -modfile=../../tools/go.mod mockgen -package certificates -destination=mocks.go github.com/gardener/oidc-apps-controller/pkg/certificates CertificateOperations
